bc571fe0551fe00ebf65f46e84ddb63d7d6d3b02
Ech0 <44973278+SpinEch0@users.noreply.github.com>
2023-09-27 10:01:09 +0800

    Update warp scheduler (#18)

--- src/main/scala/pipeline/WarpScheduler.scala
+++ src/main/scala/pipeline/WarpScheduler.scala
@@ -23,7 +23,7 @@ class WarpScheduler(implicit p: Parameters) extends Module {
   val warp_idle = RegInit(VecInit(Seq.fill(numWarps)(1.B)))
   val warp_active = RegInit(VecInit(Seq.fill(numWarps)(0.B)))
   val warp_pc = RegInit(VecInit(Seq.fill(numWarps)(0.U(addrWidth.W))))
-  val warp_tmask = RegInit(VecInit(Seq.fill(numWarps)(0.U(numThreads.W))))
+  val warp_tmask = RegInit(VecInit(Seq.fill(numWarps)(VecInit(Seq.fill(numThreads)(0.B)))))
   val pop_valid = RegInit(0.B)
   val pop_wid = RegInit(0.U(log2Ceil(numWarps).W))
   io.warp_cmd.ready := warp_idle.asUInt.orR
@@ -35,28 +35,23 @@ class WarpScheduler(implicit p: Parameters) extends Module {
   val idle_id = PriorityEncoder(warp_idle)
   val active_id = PriorityEncoder(warp_active)
 
-  val simt_diverge = VecInit(Seq.fill(numWarps)(Wire(Bool())))
-  val simt_data = VecInit(Seq.fill(numWarps)(Wire(new StackData())))
   val simt_stack = VecInit(Seq.fill(numWarps)(Module(new SIMTStack()).io))
 
   val pop_diverge = Wire(Bool())
   val pop_data = Wire(new StackData())
 
-  // 使用for循环进行赋值
   for (i <- 0 until numWarps) {
     simt_stack(i).in_diverge := io.branch_ctl.bits.wid === i.U && io.branch_ctl.valid && io.branch_ctl.bits.diverge
     simt_stack(i).in_data := io.branch_ctl.bits.data
     simt_stack(i).push := io.branch_ctl.bits.wid === i.U && io.branch_ctl.valid
     simt_stack(i).pop := io.warp_ctl.bits.wid === i.U && io.warp_ctl.valid && io.warp_ctl.bits.join
-    simt_diverge := simt_stack(i).out_diverge
-    simt_data := simt_stack(i).out_data
   }
 
   pop_valid := io.warp_ctl.valid
   pop_wid := io.warp_ctl.bits.wid
 
-  pop_diverge := simt_diverge(pop_wid)
-  pop_data := simt_data(pop_wid)
+  pop_diverge := simt_stack(pop_wid).out_diverge
+  pop_data := simt_stack(pop_wid).out_data
 
   when(io.warp_cmd.fire) {
     warp_idle(idle_id) := 0.B
@@ -80,7 +75,20 @@ class WarpScheduler(implicit p: Parameters) extends Module {
   }
 
   when(io.inst_fetch.fire) {
-    warp_active(io.inst_fetch.bits.wid) := 0.B
+    warp_active(active_id) := 0.B
+    warp_pc(active_id) := warp_pc(active_id) + 4.U
+  }
+
+  when(has_active) {
+    io.inst_fetch.valid := 1.B
+    io.inst_fetch.bits.pc := warp_pc(active_id)
+    io.inst_fetch.bits.mask := warp_tmask(active_id)
+    io.inst_fetch.bits.wid := active_id
+  }.otherwise {
+    io.inst_fetch.valid := 0.B
+    io.inst_fetch.bits.pc := 0.U
+    io.inst_fetch.bits.mask := VecInit(Seq.fill(numThreads)(0.B))
+    io.inst_fetch.bits.wid := 0.U
   }
 
 }
